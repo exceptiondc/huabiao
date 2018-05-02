b7409ed577562af35bb7b57a2609c8a6ab3626c1
Gguolin <541551546@qq.com>
2018-05-02 18:31:22 +0800

    产品图片180度旋转

--- huabiao/src/main/webapp/pc/js/product.js
+++ huabiao/src/main/webapp/pc/js/product.js
@@ -30,7 +30,42 @@ $(function(){
 		$(this).find('ol li span').hide().css('margin-top', '0');
 	});
 
-	$('.group_img ul li').hover(function(){
-		$(this).addClass('on').siblings().removeClass('on');
-	});
+	data.map(e => {
+		$('.group_img ul').append(imgList(e));
+		
+
+		var mySwiper = new Swiper ('#swiper-container' + e.id, {
+		    	loop: true,
+		    	effect : 'flip',
+		    	autoplay : false,
+		    	allowTouchMove: false,
+		  	})
+	  	$('#swiper-container' + e.id).hover(function(){
+	    	mySwiper.autoplay.start();
+	    	setTimeout(function(){
+	    		mySwiper.autoplay.stop();	
+	    	}, 500)
+	    	 
+	    }, function(){
+	    	mySwiper.autoplay.stop();
+	    })
+	})
+	 
 });
+
+var data = [{'img': '../img/product_01.png', 'id': '1'},
+			{'img': '../img/product_02.png', 'id': '2'},
+			{'img': '../img/product_03.png', 'id': '3'}, 
+			{'img': '../img/product_04.png', 'id': '4'}, 
+			{'img': '../img/product_05.png', 'id': '5'}, 
+			{'img': '../img/product_06.png', 'id': '6'}, 
+			{'img': '../img/product_07.png', 'id': '7'}];
+
+var imgList = function (data){
+	var tpl = `<div class="swiper-container" id="swiper-container${ data.id }">
+				    	<div class="swiper-wrapper">
+					        <div class="swiper-slide" data-swiper-autoplay="500"><img src="${ data.img }" alt=""></div>
+				    	</div>
+					</div>`
+		return tpl;
+}
