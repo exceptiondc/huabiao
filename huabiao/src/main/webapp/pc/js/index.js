e648e488c2811fee08073d04abe5eb3839b7ca60
Gguolin <541551546@qq.com>
2018-05-03 15:03:43 +0800

    首页布局修改，增增加我们的产品

--- huabiao/src/main/webapp/pc/js/index.js
+++ huabiao/src/main/webapp/pc/js/index.js
@@ -2,7 +2,39 @@ $(function(){
 
 	$('.header_nav ul li').eq(0).addClass('active');
 
-	//产品与方案
+	//我们的产品
+	$('.product_list ul > li').hover(function(){
+		$(this).stop().animate({
+			marginTop: '-20px',		
+		}, 600).css({
+			'width': '333px',
+			'height': 'auto',
+			'border': '1px solid #FF9C00',
+			'border-top': '3px solid #FF9C00',
+		});
+		$(this).find('ol li').css('text-align', 'left');
+		$(this).find('ol li p').css({'margin-top': '30px', 'color': '#333'});
+		$(this).find('ol li p:eq(0)').css('margin-top', '0');
+		$(this).find('ol li p b').hide();
+		$(this).find('ol li span').show().css('margin-top', '22px');
+	},function(){
+		$(this).stop().animate({
+			marginTop: '0',		
+		}, 600).css({
+			'width': '253px',
+			'height': '346px',
+			'border': '1px solid #f3f3f3',
+			'border-top': '3px solid #f3f3f3',
+			'margin-top': '0',
+		});
+		$(this).find('ol li').css('text-align', 'center');
+		$(this).find('ol li p').css({'margin-top': '0', 'line-height': '30px', 'color': '#999'});
+		$(this).find('ol li p b').show();
+		$(this).find('ol li span').hide().css('margin-top', '0');
+	});
+	
+
+	//解决方案
 	var imgUrl = "";
 	$('.programme_item ul li').hover(function(){
 		$(this).css({
